535cb7ef0e21a1c0bcfdeff77b9e123c27c1f941
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-04 22:25:49 -0500

    Update and rename flooder.js to index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-//Domano Flood Created by CPScript
+//KahootFlood is not really open source anymore so
+//We just use replit to host it.
 
 var X = M;
 (function (E, K) {
@@ -40,9 +41,9 @@ function Q() {
         'setMaxListeners',
         'POSITIVE_INFINITY',
         'clear',
-        'This\x20software\x20was\x20made\x20for\x20educational\x20and\x20demonstrational\x20purposes.\x0aDistributing\x20this\x20software\x20without\x20proper\x20credit\x20and\x20LICENSE\x20is\x20illegal.\x0a\x0aThe\x20LICENSE\x20is\x20stated\x20here\x20and\x20should\x20also\x20be\x20a\x20file\x20within\x20or\x20outside\x20the\x20program.\x0a\x0aCopyright\x20',
-        '\x20\x0a\x0aPermission\x20is\x20hereby\x20granted,\x20free\x20of\x20charge,\x20to\x20any\x20person\x20obtaining\x20a\x20copy\x20of\x20this\x20software\x20and\x20associated\x20documentation\x20files\x20(the\x20\x22Software\x22),\x20to\x20deal\x20in\x20the\x20Software\x20without\x20restriction,\x20including\x20without\x20limitation\x20the\x20rights\x20to\x20use,\x20copy,\x20modify,\x20merge,\x20publish,\x20distribute,\x20sublicense,\x20and/or\x20sell\x20copies\x20of\x20the\x20Software,\x20and\x20to\x20permit\x20persons\x20to\x20whom\x20the\x20Software\x20is\x20furnished\x20to\x20do\x20so,\x20subject\x20to\x20the\x20following\x20conditions:\x0a\x0aThe\x20above\x20copyright\x20notice\x20and\x20this\x20permission\x20notice\x20shall\x20be\x20included\x20in\x20all\x20copies\x20or\x20substantial\x20portions\x20of\x20the\x20Software.\x0a\x0aTHE\x20SOFTWARE\x20IS\x20PROVIDED\x20\x22AS\x20IS\x22,\x20WITHOUT\x20WARRANTY\x20OF\x20ANY\x20KIND,\x20EXPRESS\x20OR\x20IMPLIED,\x20INCLUDING\x20BUT\x20NOT\x20LIMITED\x20TO\x20THE\x20WARRANTIES\x20OF\x20MERCHANTABILITY,\x20FITNESS\x20FOR\x20A\x20PARTICULAR\x20PURPOSE\x20AND\x20NON-INFRINGEMENT.\x20IN\x20NO\x20EVENT\x20SHALL\x20THE\x20AUTHORS\x20OR\x20COPYRIGHT\x20HOLDERS\x20BE\x20LIABLE\x20FOR\x20ANY\x20CLAIM,\x20DAMAGES\x20OR\x20OTHER\x20LIABILITY,\x20WHETHER\x20IN\x20AN\x20ACTION\x20OF\x20CONTRACT,\x20TORT\x20OR\x20OTHERWISE,\x20ARISING\x20FROM,\x20OUT\x20OF\x20OR\x20IN\x20CONNECTION\x20WITH\x20THE\x20SOFTWARE\x20OR\x20THE\x20USE\x20OR\x20OTHER\x20DEALINGS\x20IN\x20THE\x20SOFTWARE.\x0a',
-        '>>\x20Press\x20enter\x20to\x20continue\x20<<',
+        'Creation Date-> 4/12/' ,
+        '\x20\x0a\x0a         🁞 🅓 🅞 🅜 🅘 🅝 🅞   🅕 🅛 🅞 🅞 🅓 🅔 🅡 🁞 \x0a',
+        '>>\x20Press\x20enter\x20to\x20start\x20DominoFlooder\x20<<',
         'question',
         'Is\x20the\x20',
         'host',
