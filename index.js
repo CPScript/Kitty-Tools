9db1f4422342d51787a773baf8c6efa6edb3c239
Chai <83523587+CPScript@users.noreply.github.com>
2022-12-04 20:41:56 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,374 @@
+//KahootFlood is not really open source anymore so
+//We just use replit to host it.
+
+var X = M;
+(function (E, K) {
+    var j = M, k = E();
+    while (!![]) {
+        try {
+            var p = -parseInt(j(0x0)) / 0x1 * (parseInt(j(0x1)) / 0x2) + -parseInt(j(0x2)) / 0x3 * (-parseInt(j(0x3)) / 0x4) + -parseInt(j(0x4)) / 0x5 + -parseInt(j(0x5)) / 0x6 * (parseInt(j(0x6)) / 0x7) + parseInt(j(0x7)) / 0x8 * (-parseInt(j(0x8)) / 0x9) + parseInt(j(0x9)) / 0xa * (parseInt(j(0xa)) / 0xb) + -parseInt(j(0xb)) / 0xc * (-parseInt(j(0xc)) / 0xd);
+            if (p === K)
+                break;
+            else
+                k['push'](k['shift']());
+        } catch (d) {
+            k['push'](k['shift']());
+        }
+    }
+}(Q, 0x6e0b5), console[X(0xd)](X(0xe)), readline = require('readline-sync'), console[X(0xd)](X(0xf)));
+const Kahoot = require('kahoot.js-updated');
+console[X(0xd)](X(0x10));
+function M(E, K) {
+    var k = Q();
+    return M = function (p, d) {
+        p = p - 0x0;
+        var J = k[p];
+        return J;
+    }, M(E, K);
+}
+var words = require('an-array-of-english-words');
+console[X(0xd)]('Required\x20an-array-of-english-words');
+const request = require(X(0x11));
+console[X(0xd)](X(0x12));
+var random = require('random-name');
+function Q() {
+    var v = [
+        'parse',
+        'length',
+        'first',
+        'last',
+        'random',
+        'setMaxListeners',
+        'POSITIVE_INFINITY',
+        'clear',
+        'This\x20software\x20was\x20made\x20for\x20educational\x20and\x20demonstrational\x20purposes.\x0aDistributing\x20this\x20software\x20without\x20proper\x20credit\x20and\x20LICENSE\x20is\x20illegal.\x0a\x0aThe\x20LICENSE\x20is\x20stated\x20here\x20and\x20should\x20also\x20be\x20a\x20file\x20within\x20or\x20outside\x20the\x20program.\x0a\x0aCopyright\x20',
+        '\x20\x0a\x0aPermission\x20is\x20hereby\x20granted,\x20free\x20of\x20charge,\x20to\x20any\x20person\x20obtaining\x20a\x20copy\x20of\x20this\x20software\x20and\x20associated\x20documentation\x20files\x20(the\x20\x22Software\x22),\x20to\x20deal\x20in\x20the\x20Software\x20without\x20restriction,\x20including\x20without\x20limitation\x20the\x20rights\x20to\x20use,\x20copy,\x20modify,\x20merge,\x20publish,\x20distribute,\x20sublicense,\x20and/or\x20sell\x20copies\x20of\x20the\x20Software,\x20and\x20to\x20permit\x20persons\x20to\x20whom\x20the\x20Software\x20is\x20furnished\x20to\x20do\x20so,\x20subject\x20to\x20the\x20following\x20conditions:\x0a\x0aThe\x20above\x20copyright\x20notice\x20and\x20this\x20permission\x20notice\x20shall\x20be\x20included\x20in\x20all\x20copies\x20or\x20substantial\x20portions\x20of\x20the\x20Software.\x0a\x0aTHE\x20SOFTWARE\x20IS\x20PROVIDED\x20\x22AS\x20IS\x22,\x20WITHOUT\x20WARRANTY\x20OF\x20ANY\x20KIND,\x20EXPRESS\x20OR\x20IMPLIED,\x20INCLUDING\x20BUT\x20NOT\x20LIMITED\x20TO\x20THE\x20WARRANTIES\x20OF\x20MERCHANTABILITY,\x20FITNESS\x20FOR\x20A\x20PARTICULAR\x20PURPOSE\x20AND\x20NON-INFRINGEMENT.\x20IN\x20NO\x20EVENT\x20SHALL\x20THE\x20AUTHORS\x20OR\x20COPYRIGHT\x20HOLDERS\x20BE\x20LIABLE\x20FOR\x20ANY\x20CLAIM,\x20DAMAGES\x20OR\x20OTHER\x20LIABILITY,\x20WHETHER\x20IN\x20AN\x20ACTION\x20OF\x20CONTRACT,\x20TORT\x20OR\x20OTHERWISE,\x20ARISING\x20FROM,\x20OUT\x20OF\x20OR\x20IN\x20CONNECTION\x20WITH\x20THE\x20SOFTWARE\x20OR\x20THE\x20USE\x20OR\x20OTHER\x20DEALINGS\x20IN\x20THE\x20SOFTWARE.\x0a',
+        '>>\x20Press\x20enter\x20to\x20continue\x20<<',
+        'question',
+        'Is\x20the\x20',
+        'host',
+        'NOTE:\x202-factor\x20brute\x20forcing\x20does\x20',
+        '\x20work\x20with\x20antibot\x20mode\x20enabled.',
+        'Enter\x20number\x20of\x20bots>\x20',
+        'Generate\x20random\x20names?\x20(y/n)>\x20',
+        'Enter\x20name>\x20',
+        'Use\x20name\x20bypass?\x20(y/n)>\x20',
+        'Control\x20the\x20bots?\x20(y/n)>\x20',
+        'beep',
+        '\x20when\x20the\x20bots\x20need\x20controlling?\x20(y/n)>\x20',
+        'you\x20are\x20using\x20the\x20',
+        '\x20bot\x20by\x20',
+        'kosmos',
+        'All\x20join\x20requests\x20have\x20finished.',
+        'join',
+        'replace',
+        'catch',
+        'description',
+        'Duplicate\x20name',
+        'Client\x20',
+        '\x20failed\x20to\x20join\x20with\x20the\x20error\x20\x27',
+        'leave',
+        '\x20is\x20trying\x20to\x20crack\x20the\x202\x20factor',
+        'answerTwoFactorAuth',
+        'TwoFactorCorrect',
+        '\x20cracked\x20the\x202-factor!',
+        'QuestionReady',
+        'type',
+        'word_cloud',
+        'Type\x20your\x20answer>\x20',
+        'answer',
+        '\x20answered\x20with\x20\\x27',
+        '',
+        'jumble',
+        'quizQuestionAnswers',
+        'questionIndex',
+        'quiz',
+        't\x20for\x20triangle,\x20d\x20for\x20diamond>\x20',
+        't\x20for\x20triangle,\x20d\x20for\x20diamond,\x20c\x20for\x20circle>\x20',
+        '\x20answered\x20with\x20a\x20random\x20answer.',
+        't\x20for\x20triangle,\x20d\x20for\x20diamond,\x20c\x20for\x20circle\x20or\x20s\x20for\x20square\x20>\x20',
+        'open_ended',
+        'Disconnect',
+        'Quiz\x20Locked',
+        'Preparing\x20to\x20rejoin\x20when\x20quiz\x20is\x20unlocked',
+        'QuestionEnd',
+        'isCorrect',
+        'QuizEnd',
+        '\x20got\x20',
+        'rank',
+        'SIGINT',
+        'exit',
+        'Flooding\x20bots...',
+        '654275AUllbK',
+        '2AphKSc',
+        '291411jwIaUP',
+        '20EpEkhl',
+        '1511890qtupNX',
+        '76704OAgRBy',
+        '21SAjwMp',
+        '904sxVnFL',
+        '22527BfAwVQ',
+        '20aFPQSv',
+        '2963719VaclNx',
+        '12oknLEJ',
+        '9152546VCvPyj',
+        'log',
+        'Preparing...',
+        'Required\x20readline-sync',
+        'Required\x20kahoot.js-updated',
+        'request',
+        'Required\x20request',
+        'Required\x20random-name',
+        'console-title',
+        'Kahoot\x20nuker\x20|\x20kosmos',
+        'beepbeep',
+        'Required\x20beepbeep',
+        'colors',
+        'Required\x20colors',
+        'floor'
+    ];
+    Q = function () {
+        return v;
+    };
+    return Q();
+}
+console['log'](X(0x13));
+var setTitle = require(X(0x14));
+console[X(0xd)]('Required\x20console-title'), setTitle(X(0x15));
+var beep = require(X(0x16));
+console['log'](X(0x17));
+var colors = require(X(0x18));
+console['log'](X(0x19));
+function getRandomInt(E, K) {
+    var Y = X;
+    return E = Math['ceil'](E), K = Math['floor'](K), Math[Y(0x1a)](Math['random']() * (K - E + 0x1)) + E;
+}
+function getName() {
+    var I = X;
+    ran = getRandomInt(0x1, 0x5);
+    ran == 0x5 && request('https://apis.kahoot.it/namerator', function (E, K, k) {
+        var o = M;
+        return E && console['log'](E), JSON[o(0x1b)](k)['name'];
+    });
+    if (ran == 0x4)
+        return words[getRandomInt(0x1, words[I(0x1c)])];
+    if (ran == 0x3)
+        return random[I(0x1d)]();
+    if (ran == 0x2)
+        return random[I(0x1d)]() + '\x20' + random['middle']() + '\x20' + random[I(0x1e)]();
+    if (ran == 0x1)
+        return random[I(0x1d)]() + '\x20' + random[I(0x1e)]();
+}
+function shuffle(E) {
+    var U = X, K = E[U(0x1c)], k, p;
+    while (0x0 !== K) {
+        p = Math['floor'](Math[U(0x1f)]() * K), K -= 0x1, k = E[K], E[K] = E[p], E[p] = k;
+    }
+    return E;
+}
+process[X(0x20)](Number[X(0x21)]), console[X(0x22)](), console[X(0xd)](X(0x23) + new Date()['getFullYear']() + X(0x24)), readline['question'](X(0x25));
+function ads() {
+    var q = X;
+}
+ads(), antibotmode = readline[X(0x26)](X(0x27) + X(0x28) + '\x20using\x20an\x20anti-bot\x20program?\x20(y/n)>\x20');
+antibotmode == 'y' && console[X(0xd)](X(0x29) + 'not' + X(0x2a));
+pin = readline[X(0x26)]('Enter\x20game\x20pin>\x20'), bots = readline[X(0x26)](X(0x2b));
+antibotmode == 'y' ? (ranname = !![], er = 'n') : (ranname = readline[X(0x26)](X(0x2c)), ranname == 'y' ? ranname = !![] : (ranname = ![], botname = readline[X(0x26)](X(0x2d)), botprefix = ''), er = readline[X(0x26)](X(0x2e)));
+usercontrolled = readline['question'](X(0x2f));
+usercontrolled == 'y' ? (usercontrolled = !![], beepis = readline[X(0x26)]('Shall\x20I\x20' + X(0x30) + X(0x31))) : (usercontrolled = ![], beepis = 'n');
+console[X(0x22)](), setInterval(() => {
+    var h = X;
+    console[h(0xd)](h(0x32) + 'kosmos flooder ez/' + h(0x33) + h(0x34) + '.');
+}, 0x2710), repeattimes = 0x0;
+function ordinal_suffix_of(E) {
+    var K = E % 0xa, p = E % 0x64;
+    if (K == 0x1 && p != 0xb)
+        return E + 'st';
+    if (K == 0x2 && p != 0xc)
+        return E + 'nd';
+    if (K == 0x3 && p != 0xd)
+        return E + 'rd';
+    return E + 'th';
+}
+function sendjoin(E, K) {
+    ranname ? join(getName(), K) : join(E, K);
+}
+function spam() {
+    var D = X;
+    repeattimes == bots ? console['log'](D(0x35)) : (repeattimes++, ranname ? rt = getRandomInt(0x5a, 0xc8) : rt = 0xf, setTimeout(() => {
+        spam();
+    }, rt), setTimeout(() => {
+        ranname ? sendjoin('This\x20will\x20become\x20a\x20random\x20name!', bots - repeattimes - 0x1) : sendjoin(botname + (bots - repeattimes - 0x1), bots - repeattimes - 0x1);
+    }, rt));
+}
+process['setMaxListeners'](Number[X(0x21)]), Arandomint = 0x0, todo = ![];
+function join(E, K) {
+    var L = X;
+    while (E == undefined) {
+        E = getName();
+    }
+    const k = new Kahoot();
+    k['setMaxListeners'](Number[L(0x21)]);
+    er == 'y' ? k[L(0x36)](pin, E[L(0x37)](/a/g, 'ᗩ')['replace'](/b/g, 'ᗷ')[L(0x37)](/c/g, 'ᑕ')[L(0x37)](/d/g, 'ᗪ')[L(0x37)](/e/g, 'E')[L(0x37)](/f/g, 'ᖴ')[L(0x37)](/g/g, 'G')['replace'](/h/g, 'ᕼ')['replace'](/i/g, 'I')['replace'](/j/g, 'ᒍ')['replace'](/k/g, 'K')[L(0x37)](/l/g, 'ᒪ')[L(0x37)](/m/g, 'ᗰ')[L(0x37)](/n/g, 'ᑎ')[L(0x37)](/o/g, 'O')['replace'](/p/g, 'ᑭ')[L(0x37)](/q/g, 'ᑫ')[L(0x37)](/r/g, 'ᖇ')[L(0x37)](/s/g, 'ᔕ')[L(0x37)](/t/g, 'T')['replace'](/u/g, 'ᑌ')[L(0x37)](/v/g, 'ᐯ')[L(0x37)](/w/g, 'ᗯ')[L(0x37)](/x/g, '᙭')[L(0x37)](/y/g, 'Y')[L(0x37)](/z/g, 'ᘔ')['replace'](/A/g, 'ᗩ')[L(0x37)](/B/g, 'ᗷ')[L(0x37)](/C/g, 'ᑕ')[L(0x37)](/D/g, 'ᗪ')['replace'](/E/g, 'E')['replace'](/F/g, 'ᖴ')[L(0x37)](/G/g, 'G')['replace'](/H/g, 'ᕼ')[L(0x37)](/I/g, 'I')['replace'](/J/g, 'ᒍ')[L(0x37)](/K/g, 'K')[L(0x37)](/L/g, 'ᒪ')[L(0x37)](/M/g, 'ᗰ')[L(0x37)](/N/g, 'ᑎ')[L(0x37)](/O/g, 'O')[L(0x37)](/P/g, 'ᑭ')[L(0x37)](/Q/g, 'ᑫ')[L(0x37)](/R/g, 'ᖇ')['replace'](/S/g, 'ᔕ')[L(0x37)](/T/g, 'T')['replace'](/U/g, 'ᑌ')[L(0x37)](/V/g, 'ᐯ')['replace'](/W/g, 'ᗯ')['replace'](/X/g, '᙭')[L(0x37)](/Y/g, 'Y')[L(0x37)](/Z/g, 'ᘔ'), [
+        random[L(0x1d)](),
+        random[L(0x1e)]()
+    ])[L(0x38)](d => {
+        var b = L;
+        d[b(0x39)] == b(0x3a) & ranname ? sendjoin(E, K) : (console[b(0xd)](b(0x3b) + K + b(0x3c) + d[b(0x39)] + '\x27'), k[b(0x3d)]());
+    }) : k[L(0x36)](pin, E, [
+        random[L(0x1d)](),
+        random['last']()
+    ])['catch'](d => {
+        var a = L;
+        d['description'] == a(0x3a) & ranname ? sendjoin(E, K) : (console['log'](a(0x3b) + K + a(0x3c) + d['description'] + '\x27'), k['leave']());
+    });
+    var p = [
+        0x0,
+        0x1,
+        0x2,
+        0x3
+    ];
+    todo = ![], k['on']('Joined', d => {
+        var S = L;
+        if (d['twoFactorAuth'])
+            console[S(0xd)](E + S(0x3e)), setInterval(() => {
+                var g = S;
+                !todo == ![] && k[g(0x3f)](todo), shuffle(p), setTimeout(function () {
+                    var y = g;
+                    k[y(0x3f)](p);
+                }, 0xfa);
+            }, 0x1f4);
+        else {
+        }
+    }), k['on'](L(0x40), function () {
+        var Z = L;
+        console['log'](E + Z(0x41)), todo = p;
+    }), k['on'](L(0x42), d => {
+        var x = L;
+        K == 0x1 & beepis == 'y' && beep();
+        everyoneanswerthis = ![];
+        if (d[x(0x43)] == x(0x44)) {
+            if (usercontrolled) {
+                if (K == 0x1)
+                    everyoneanswerthis = !![], answer = readline[x(0x26)](x(0x45)), everyoneanswerthis = answer, Arandomint = answer, setTimeout(() => {
+                        k['answer'](answer - 0x1);
+                    }, getRandomInt(0x1, 0x4e20));
+                else
+                    var J = setInterval(() => {
+                        (!everyoneanswerthis == ![] || !everyoneanswerthis == !![]) && (clearInterval(J), setTimeout(() => {
+                            var V = M;
+                            k[V(0x46)](Arandomint);
+                        }, getRandomInt(0x1, 0x4e20)));
+                    }, 0x64);
+            } else
+                console['log'](E + x(0x47)), setTimeout(() => {
+                    var C = x;
+                    k[C(0x46)](C(0x48));
+                }, getRandomInt(0x1, 0x4e20));
+        }
+        d[x(0x43)] == x(0x49) && (console[x(0xd)]('User\x20controlling\x20is\x20not\x20available\x20for\x20jumbles.\x20The\x20bot\x20' + E + '\x20responded\x20with\x20a\x20random\x20answer.'), setTimeout(() => {
+            var T = x;
+            k[T(0x46)](getRandomInt(0x0, d[T(0x4a)][d[T(0x4b)]] - 0x1));
+        }, getRandomInt(0x1, 0x4e20)));
+        if (d[x(0x43)] == x(0x4c)) {
+            if (usercontrolled) {
+                if (d[x(0x4a)][d[x(0x4b)]] == 0x2) {
+                    if (K == 0x1)
+                        everyoneanswerthis = !![], answer = readline[x(0x26)](x(0x4d)), answer = answer['replace']('t', 0x1)[x(0x37)]('d', 0x2), everyoneanswerthis = answer, Arandomint = answer, setTimeout(() => {
+                            var i = x;
+                            k[i(0x46)](answer - 0x1);
+                        }, getRandomInt(0x1, 0x4e20));
+                    else
+                        var J = setInterval(() => {
+                            (!everyoneanswerthis == ![] || !everyoneanswerthis == !![]) && (clearInterval(J), setTimeout(() => {
+                                k['answer'](Arandomint - 0x1);
+                            }, getRandomInt(0x1, 0x4e20)));
+                        }, 0x64);
+                }
+                if (d[x(0x4a)][d['questionIndex']] == 0x3) {
+                    if (K == 0x1)
+                        everyoneanswerthis = !![], answer = readline[x(0x26)](x(0x4e)), answer = answer[x(0x37)]('t', 0x1)[x(0x37)]('d', 0x2)['replace']('c', 0x3), everyoneanswerthis = answer, Arandomint = answer, setTimeout(() => {
+                            var H = x;
+                            k[H(0x46)](answer - 0x1);
+                        }, getRandomInt(0x1, 0x4e20));
+                    else
+                        var J = setInterval(() => {
+                            (!everyoneanswerthis == ![] || !everyoneanswerthis == !![]) && (clearInterval(J), setTimeout(() => {
+                                var l = M;
+                                k[l(0x46)](Arandomint - 0x1);
+                            }, getRandomInt(0x1, 0x4e20)));
+                        }, 0x64);
+                }
+                if (d[x(0x4a)][d[x(0x4b)]] == 0x4) {
+                    if (K == 0x1)
+                        everyoneanswerthis = !![], answer = readline[x(0x26)]('t\x20for\x20triangle,\x20d\x20for\x20diamond,\x20c\x20for\x20circle\x20or\x20s\x20for\x20square\x20>\x20'), answer = answer[x(0x37)]('t', 0x1)[x(0x37)]('d', 0x2)[x(0x37)]('c', 0x3)[x(0x37)]('s', 0x4), everyoneanswerthis = answer, Arandomint = answer, setTimeout(() => {
+                            var w = x;
+                            k[w(0x46)](answer - 0x1);
+                        }, getRandomInt(0x1, 0x4e20));
+                    else
+                        var J = setInterval(() => {
+                            (!everyoneanswerthis == ![] || !everyoneanswerthis == !![]) && (clearInterval(J), setTimeout(() => {
+                                var N = M;
+                                k[N(0x46)](Arandomint - 0x1);
+                            }, getRandomInt(0x1, 0x4e20)));
+                        }, 0x64);
+                }
+            } else
+                setTimeout(() => {
+                    var f = x;
+                    toanswer = getRandomInt(0x0, d['quizQuestionAnswers'][d[f(0x4b)]] - 0x1), console[f(0xd)](E + f(0x4f)), k[f(0x46)](toanswer);
+                }, getRandomInt(0x1, 0x4e20));
+        }
+        if (d[x(0x43)] == 'survey') {
+            if (usercontrolled) {
+                if (K == 0x1)
+                    everyoneanswerthis = !![], answer = readline[x(0x26)](x(0x50)), answer = answer['replace']('t', 0x1)['replace']('d', 0x2)[x(0x37)]('c', 0x3)[x(0x37)]('s', 0x4), everyoneanswerthis = answer, Arandomint = answer, setTimeout(() => {
+                        k['answer'](answer - 0x1);
+                    }, getRandomInt(0x1, 0x4e20));
+                else
+                    var J = setInterval(() => {
+                        (!everyoneanswerthis == ![] || !everyoneanswerthis == !![]) && (clearInterval(J), setTimeout(() => {
+                            var u = M;
+                            k[u(0x46)](Arandomint - 0x1);
+                        }, getRandomInt(0x1, 0x4e20)));
+                    }, 0x64);
+            } else
+                setTimeout(() => {
+                    var A = x;
+                    toanswer = getRandomInt(0x0, d[A(0x4a)][d[A(0x4b)]] - 0x1), console['log'](E + '\x20answered\x20with\x20a\x20random\x20answer.'), k[A(0x46)](toanswer);
+                }, getRandomInt(0x1, 0x4e20));
+        }
+        if (d[x(0x43)] == x(0x51)) {
+            if (usercontrolled) {
+                if (K == 0x1)
+                    everyoneanswerthis = !![], answer = readline[x(0x26)]('Type\x20your\x20answer>\x20'), everyoneanswerthis = answer, Arandomint = answer, setTimeout(() => {
+                        k['answer'](answer - 0x1);
+                    }, getRandomInt(0x1, 0x4e20));
+                else
+                    var J = setInterval(() => {
+                        (!everyoneanswerthis == ![] || !everyoneanswerthis == !![]) && (clearInterval(J), setTimeout(() => {
+                            var z = M;
+                            k[z(0x46)](Arandomint);
+                        }, getRandomInt(0x1, 0x4e20)));
+                    }, 0x64);
+            } else
+                console['log'](E + '\x20answered\x20with\x20\x27kahootflood.weebly.com\x27'), setTimeout(() => {
+                    var F = x;
+                    k[F(0x46)](F(0x48));
+                }, getRandomInt(0x1, 0x4e20));
+        }
+    }), k['on'](L(0x52), d => {
+        var r = L;
+        sendjoin(E, K), !d == r(0x53) && (console['log'](r(0x54)), sendjoin(E, K));
+    }), k['on'](L(0x55), d => {
+        var c = L;
+        d[c(0x56)] ? console['log'](E + '\x20Got\x20it\x20correct!') : console['log'](E + '\x20Got\x20it\x20wrong.');
+    }), k['on'](L(0x57), d => {
+        var W = L;
+        console[W(0xd)](E + W(0x58) + ordinal_suffix_of(d[W(0x59)]));
+    }), process['on'](L(0x5a), function () {
+        var B = L;
+        k[B(0x3d)](), console[B(0xd)](E + '\x20is\x20leaving\x20because\x20of\x20a\x20SIGINT\x20request.'), process[B(0x5b)]();
+    });
+}
+console[X(0x22)](), console[X(0xd)](X(0x5c)), spam();
