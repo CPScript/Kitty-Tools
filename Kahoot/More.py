07be59c2853ce48b6850e90f8788ea5acb3d61cc
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-23 09:27:03 -0500

    Update More.py

--- Kahoot/More.py
+++ Kahoot/More.py
@@ -4,7 +4,9 @@ import sys
 from scripts.sprint import sprint
 from scripts.colors import ran,y,r,g,c
 import time
-print("Nothing is here")
+sprint(f"{y}Nothing is here...")
+time.sleep(2)
+sprint(f"{r}NOTHING AT ALL...{c}")
 time.sleep(5)
 
 from subprocess import call
