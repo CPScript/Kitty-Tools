f6d766ee63b9e43614ad97680ea6da240c48f84f
Chai <83523587+CPScript@users.noreply.github.com>
2023-01-23 14:06:33 -0500

    Update More.py

--- Kahoot/More.py
+++ Kahoot/More.py
@@ -7,6 +7,8 @@ import time
 sprint(f"{y}Nothing is here...")
 time.sleep(2)
 sprint(f"{r}NOTHING AT ALL...{c}")
+time.sleep(2)
+sprint(f"{r}ALSO thanks for the 3000+ veiws on This repository... iv never been more happy about something iv done when programming <3{c}")
 time.sleep(5)
 
 from subprocess import call
