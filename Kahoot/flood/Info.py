80086010b539389a40491f758c34d9663ccb4dc4
Chai <83523587+CPScript@users.noreply.github.com>
2023-04-12 08:43:57 -0400

    Update Info.py

--- Kahoot/flood/Info.py
+++ Kahoot/flood/Info.py
@@ -6,13 +6,12 @@ time.sleep(2)
 
 
 print("\n" * 128)
-sprint(f"{r}!!!REQUIRMENTS!!!")
-sprint(f"{r}MUST Have {y} Chrome Drivers installed, They can be found in the {r}Kahoot/flood/chromedriver' {y}Directory/file\n")
+print(f"""{y}
+""")
 sprint(f"{r}!!!WARNING!!!")
-sprint(f"{r}CPScript {y}is not responsible for the {r}user's actions{y}, and {r}they agree to this by typing {c}'yes' {y}below")
-sprint(f"{y}If this code is ran in a sandbox, it wont work correctly, and might not work at all...")
+print(f"{r}CPScript {y}is not responsible for the {r}user's actions{y}, and {r}they agree to this by typing {c}'yes' {y}below")
+print(f"{y}If this code is ran in a sandbox, it wont work correctly, and might not work at all...")
 
-sprint(f"Please use the newest verison of PYTHON\n")
 print("Start? (TYPE FULL WORD)")
 print("yes | no")
 choice = input("").lower()
@@ -21,11 +20,7 @@ if choice == "yes":
     print("\nStarting...")
     time.sleep(1)
     print("\n" * 64)
-    call(["python", "Kahoot/flood/main.py"])
-    
-    print("Thank you for using Kahoot Flooder!")
-    time.sleep(5)
-    call(["python", "main.py"])
+    execfile('index.js')
     
 if choice == "no":
     print("\nLoading...")
