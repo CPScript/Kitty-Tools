b58e2f61f5feffa4cf6044545062c4662c201d61
Disease <83523587+CPScript@users.noreply.github.com>
2023-12-07 18:46:31 -0500

    Update kahoot.py

--- Kahoot/kahoot.py
+++ Kahoot/kahoot.py
@@ -2,13 +2,13 @@ import json
 import urllib.request
 import os
 
-print("         ▄▄ ▄████▄▐▄▄▄▌           ")
-print("        ▐  ████▀███▄█▄▌           ")
-print("      ▐ ▌  █▀▌  ▐▀▌▀█▀            ")
-print("       ▀   ▌ ▌  ▐ ▌               ")
-print("           █ █  ▐▌█               ")
-print("    --- Please support ---        ")
-print("Follow me on github: CPScript   ")
+print("                    ▄▄ ▄████▄▐▄▄▄▌           ")
+print("                   ▐  ████▀███▄█▄▌           ")
+print("                 ▐ ▌  █▀▌  ▐▀▌▀█▀            ")
+print("                  ▀   ▌ ▌  ▐ ▌               ")
+print("                      █ █  ▐▌█               ")
+print("               --- Please support ---                 ")
+print("Want to donate? Send me 1$ on cashapp - $LovelyDisease")
 print(" ")
 print("______________________________")
 
