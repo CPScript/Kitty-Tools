f5abf208f25c37bd72f10efde99be4dbb3a9bd37
Disease <83523587+CPScript@users.noreply.github.com>
2024-02-12 16:56:03 -0500

    Create extra.py

---
+++ Kahoot/Info/extra.py
@@ -0,0 +1,15 @@
+import os
+import platform
+import sys
+from scripts.sprint import sprint
+from scripts.colors import ran,y,r,g,c
+import time
+sprint(f"{y}Nothing is here...")
+time.sleep(2)
+sprint(f"{r}NOTHING AT ALL...{c}")
+time.sleep(2)
+sprint(f"{r}THANKS {c}for the veiws on This repository... iv never been more happy about something iv done when programming <3{c}")
+time.sleep(5)
+
+from subprocess import call
+call(["python", "main.py"])
